2e46d98cd9af3864dd0b1d82f4ab8476b7966cf6
BDOR665 <165445587+BDOR665@users.noreply.github.com>
2025-03-24 17:12:09 +0900

    Update 03_SUBQUERY_practice.sql
    change

--- 장윤영/03_SUBQUERY_practice.sql
+++ 장윤영/03_SUBQUERY_practice.sql
@@ -4,48 +4,49 @@ SELECT EMP_NAME
 	WHERE DEPT_CODE = (
 		SELECT DEPT_CODE
 			FROM EMPLOYEE
-            WHERE EMP_NAME = '노옹철');
+            		WHERE EMP_NAME = '노옹철');
 
 -- 2. 전 직원의 평균 급여보다 많은 급여를 받고 있는 직원의 사번, 이름, 직급코드, 급여를 조회하세요.
 SELECT EMP_ID, EMP_NAME, JOB_CODE, SALARY
 	FROM EMPLOYEE
-    WHERE SALARY > (SELECT AVG(SALARY)
-						FROM EMPLOYEE);
+    	WHERE SALARY > (SELECT AVG(SALARY)
+				FROM EMPLOYEE);
 
 -- 3. 노옹철 사원의 급여보다 많이 받는 직원의 사번, 이름, 부서코드, 직급코드, 급여를 조회하세요.
 SELECT EMP_ID, EMP_NAME, DEPT_CODE, JOB_CODE, SALARY
 	FROM EMPLOYEE
-    WHERE SALARY > (SELECT SALARY
-						FROM EMPLOYEE
-                        WHERE EMP_NAME = '노옹철');
+   	WHERE SALARY > (SELECT SALARY
+				FROM EMPLOYEE
+                       		WHERE EMP_NAME = '노옹철');
 
 -- 4. 가장 적은 급여를 받는 직원의 사번, 이름, 부서코드, 직급코드, 급여, 입사일을 조회하세요.
 SELECT EMP_ID, EMP_NAME, DEPT_CODE, JOB_CODE, SALARY, HIRE_DATE
 	FROM EMPLOYEE
-    WHERE SALARY = (SELECT MIN(SALARY)
-						FROM EMPLOYEE);
+   	 WHERE SALARY = (SELECT MIN(SALARY)
+				FROM EMPLOYEE);
 
 -- *** 서브쿼리는 SELECT, FROM, WHERE, HAVING, ORDER BY절에도 사용할 수 있다.
 
 -- 5. 부서별 최고 급여를 받는 직원의 이름, 직급코드, 부서코드, 급여 조회하세요.
 SELECT EMP_NAME, JOB_CODE, DEPT_CODE, SALARY
 	FROM EMPLOYEE
-    WHERE SALARY IN (SELECT MAX(SALARY)
-						FROM EMPLOYEE
-                        GROUP BY DEPT_CODE);
+   	WHERE SALARY IN (SELECT MAX(SALARY)
+				FROM EMPLOYEE
+                        	GROUP BY DEPT_CODE);
 
 -- *** 여기서부터 난이도 극상
 
 -- 6. 관리자에 해당하는 직원에 대한 정보와 관리자가 아닌 직원의 정보를 추출하여 조회하세요.
 -- 사번, 이름, 부서명, 직급, '관리자' AS 구분 / '직원' AS 구분
 -- HINT!! is not null, union(혹은 then, else), distinctdepartment
-SELECT E.EMP_ID AS '사번', E.EMP_NAME AS '이름', D.DEPT_TITLE AS '부서명', J.JOB_NAME AS '직급', CASE
-																								WHEN E.EMP_ID IN (
-                                                                                                SELECT DISTINCT MANAGER_ID
-																									FROM EMPLOYEE
-                                                                                                    WHERE MANAGER_ID IS NOT NULL) THEN '관리자'
-																								ELSE '직원'
-																							END AS '구분'
+SELECT E.EMP_ID AS '사번', E.EMP_NAME AS '이름', D.DEPT_TITLE AS '부서명', J.JOB_NAME AS '직급', 
+	CASE
+		WHEN E.EMP_ID IN (
+		SELECT DISTINCT MANAGER_ID
+		FROM EMPLOYEE
+		WHERE MANAGER_ID IS NOT NULL) THEN '관리자'
+						ELSE '직원'
+					END AS '구분'
 	FROM EMPLOYEE E
     LEFT JOIN DEPARTMENT D ON D.DEPT_ID = E.DEPT_CODE
     LEFT JOIN JOB J ON J.JOB_CODE = E.JOB_CODE
@@ -57,8 +58,8 @@ SELECT E.EMP_ID AS '사번', E.EMP_NAME AS '이름', D.DEPT_TITLE AS '부서명'
 SELECT EMP_ID, EMP_NAME, JOB_CODE, SALARY
 	FROM EMPLOYEE E
     WHERE ROUND(SALARY, -5) IN (SELECT ROUND(AVG(SALARY), -5)
-									FROM EMPLOYEE
-									GROUP BY JOB_CODE = E.JOB_CODE);
+	FROM EMPLOYEE
+	GROUP BY JOB_CODE = E.JOB_CODE);
 
 SELECT 
     JOB_CODE,
@@ -72,8 +73,8 @@ ORDER BY JOB_CODE;
 SELECT EMP_NAME, JOB_CODE, DEPT_CODE, HIRE_DATE
 	FROM EMPLOYEE
     WHERE (DEPT_CODE, JOB_CODE) IN (SELECT DEPT_CODE, JOB_CODE
-									FROM EMPLOYEE
-									WHERE ENT_DATE IS NOT NULL) AND ENT_DATE IS NULL;
+					FROM EMPLOYEE
+					WHERE ENT_DATE IS NOT NULL) AND ENT_DATE IS NULL;
 
 -- 9. 급여 평균 3위 안에 드는 부서의 부서 코드와 부서명, 평균급여를 조회하세요.
 -- HINT!! limit
@@ -89,4 +90,4 @@ SELECT D.DEPT_TITLE, SUM(E.SALARY)
     JOIN DEPARTMENT D ON D.DEPT_ID = E.DEPT_CODE
     GROUP BY D.DEPT_TITLE
     HAVING SUM(E.SALARY) > (SELECT SUM(SALARY) * 0.2
-								FROM EMPLOYEE);
+				FROM EMPLOYEE);
