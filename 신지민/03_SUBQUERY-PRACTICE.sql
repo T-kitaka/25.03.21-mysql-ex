b89b26bf1777f19359ad2e8056b56835104cef26
Jimin730 <dnddnd120@naver.com>
2025-03-24 17:53:06 +0900

    update 코드 수정

--- 신지민/03_SUBQUERY-PRACTICE.sql
+++ 신지민/03_SUBQUERY-PRACTICE.sql
@@ -2,7 +2,7 @@
 select emp_name
 from employee
 where dept_code = (select dept_code 
-		   from employee 
+				   from employee 
                    where emp_name = '노옹철');
 
 
@@ -10,14 +10,14 @@ where dept_code = (select dept_code
 select emp_id, emp_name, job_code, salary
 from employee
 where salary > (select avg(salary) 
-		from employee);
+				from employee);
 
 
 -- 3. 노옹철 사원의 급여보다 많이 받는 직원의 사번, 이름, 부서코드, 직급코드, 급여를 조회하세요.
 select emp_id, emp_name, dept_code, job_code, salary
 from employee
 where salary > (select salary 
-		from employee 
+				from employee 
                 where emp_name = '노옹철');
 
 
@@ -25,7 +25,7 @@ where salary > (select salary
 select emp_id, emp_name, dept_code, job_code, salary, hire_date
 from employee
 where salary = (select min(salary) 
-		from employee);
+				from employee);
 
 
 -- *** 서브쿼리는 SELECT, FROM, WHERE, HAVING, ORDER BY절에도 사용할 수 있다.
@@ -34,7 +34,7 @@ where salary = (select min(salary)
 select emp_name, job_code, dept_code, salary
 from employee
 where salary in (select max(salary) 
-		 from employee 
+				 from employee 
                  group by dept_code);
 
 
@@ -46,7 +46,7 @@ where salary in (select max(salary)
 -- manager id is not null
 select e.emp_id, e.emp_name, d.dept_title, j.job_name,
 case when e.emp_id in (select distinct manager_id 
-		       from employee 
+					   from employee 
                        where manager_id is not null)
      then '관리자'
      else '직원'
@@ -59,16 +59,16 @@ order by 구분;
 -- 7. 자기 직급의 평균 급여를 받고 있는 직원의 사번, 이름, 직급코드, 급여를 조회하세요.
 -- 단, 급여와 급여 평균은 만원단위로 계산하세요.
 -- HINT!! round(컬럼명, -5)
-select e.emp_id, e.emp_name, e.job_code, e.salary
+select e.emp_id, e.emp_name, e.job_code, round(e.salary, -5) as salary
 from employee e
 join (select job_code, round(avg(salary), -5) as avg_salary 
       from employee 
       group by job_code) as a
 on e.job_code = a.job_code
-where e.salary = a.avg_salary;
+where round(e.salary, -5) = a.avg_salary;
 
 -- 직급별 평균 급여 
--- select job_code, round(avg(salary), -5) as avg_salary from employee group by job_code;
+select job_code, round(avg(salary), -5) as avg_salary from employee group by job_code;
 
 -- 8. 퇴사한 여직원과 같은 부서, 같은 직급에 해당하는 직원의 이름, 직급코드, 부서코드, 입사일을 조회하세요.
 select e1.emp_name, e1.job_code, e1.dept_code, e1.hire_date
@@ -79,7 +79,7 @@ join (select job_code, dept_code
 where e1.job_code = e2.job_code 
   and e1.dept_code = e2.dept_code
   and e1.emp_id not in(select emp_id 
-		       from employee 
+		               from employee 
                        where ent_yn = 'Y'); 
 
 
@@ -99,4 +99,4 @@ from employee e
 join department d on e.dept_code = d.dept_id
 group by e.dept_code
 having sum(e.salary) > (select round(sum(salary)*0.2, -5) 
-			from employee);
+						from employee);
